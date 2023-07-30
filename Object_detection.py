85e861809a14fc13a61595dc496a2d57b4707045
aalexa201222 <antreas.loui.alexandrou@gmail.com>
2023-07-30 17:56:33 +0300

    making the output responsive (to work with different image sizes)

--- Object_detection.py
+++ Object_detection.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-image_path = 'room.jpg'
+image_path = 'r.jpg'
 
 prototxt_path = 'models/MobileNetSSD_deploy.prototxt'
 model_path = 'models/MobileNetSSD_deploy.caffemodel'
@@ -40,7 +40,12 @@ detected_object = net.forward()
 print(detected_object[0][0][0]) #data of the first object detected, detected_object[0][0][1] is the second etc
 print(detected_object.shape[3])
 
+thickness_scale = image.shape[0] / 1000  # Scale the thickness based on the height
+font_scale = 2.0 * thickness_scale  # Scale the font size based on the thickness
+text_position_scale = image.shape[0] / 1000  # Scale the text position based on the height
 
+thickness = 2
+font_scale = .5
 ##CHECK ALL THE PREDICTIONS -> CHOSE THE ONES WITH GOOD PREDICTION VALUE -> SET THE VISUALISATION
 for i in range(detected_object.shape[2]):
     #how confident is the model about the object that detected in the img
@@ -56,12 +61,25 @@ for i in range(detected_object.shape[2]):
         lower_right_x = int(detected_object[0, 0, i, 5] * width)
         lower_right_y = int(detected_object[0, 0, i,6] * height)
 
-        prediction_text =f" {classes[class_index]}: {confidence:.2f}%"
-        cv2.rectangle(image,(upper_left_x,upper_left_y),(lower_right_x,lower_right_y),colors[class_index],3)
+        prediction_text =f" {classes[class_index]}: {int(confidence*100)}%"
+        # Calculate the scaled thickness
+        scaled_thickness = int(thickness * thickness_scale)
+        # Calculate the scaled font size
+        scaled_font_size = font_scale * thickness_scale
+        # 15 is the distance from the box for the text
+        scaled_text_position = int(15 * text_position_scale)
+
+        cv2.rectangle(image,(upper_left_x,upper_left_y),(lower_right_x,lower_right_y),colors[class_index],scaled_thickness)
         if upper_left_y > 30:
-            cv2.putText(image, prediction_text,(upper_left_x,upper_left_y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors[class_index],2)
+            cv2.putText(image, prediction_text,(upper_left_x,upper_left_y - scaled_text_position), cv2.FONT_HERSHEY_SIMPLEX, scaled_font_size, colors[class_index],scaled_thickness)
         else:
-            cv2.putText(image, prediction_text, (upper_left_x, upper_left_y +15),cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors[class_index],2)
-cv2.imshow(f"Detected Objects from the image named {image_path}", image)
+            cv2.putText(image, prediction_text, (upper_left_x, upper_left_y + scaled_text_position),cv2.FONT_HERSHEY_SIMPLEX, scaled_font_size, colors[class_index],scaled_thickness)
+# resized_image = cv2.resize(image, (1500, 1000))
+desired_width = 1500
+aspect_ratio = image.shape[1] / image.shape[0]
+desired_height = int(desired_width / aspect_ratio)
+
+resized_image = cv2.resize(image, (desired_width, desired_height))
+cv2.imshow(f"Detected Objects from the image named {image_path}", resized_image)
 cv2.waitKey(0) #close the picture with any key
 cv2.destroyAllWindows() #destroy all the related windows
